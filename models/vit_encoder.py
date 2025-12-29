4dd9e33efbdfbcf7d706512cb478e75557f4beb8
Vivek Palsutkar <139462948+Astatine25@users.noreply.github.com>
2025-12-29 15:33:33 +0530

    Refactor ViTEncoder to use emb_dim and proj layer

--- models/vit_encoder.py
+++ models/vit_encoder.py
@@ -1,14 +1,17 @@
 import timm
+import torch
 import torch.nn as nn
 
 class ViTEncoder(nn.Module):
-    def __init__(self, embed_dim=128):
+    def __init__(self, emb_dim=64):
         super().__init__()
         self.vit = timm.create_model(
-            "vit_tiny_patch16_224", pretrained=True
+            "vit_base_patch16_224",
+            pretrained=True,
+            num_classes=0
         )
-        self.vit.reset_classifier(0)
-        self.fc = nn.Linear(self.vit.num_features, embed_dim)
+        self.proj = nn.Linear(self.vit.num_features, emb_dim)
 
     def forward(self, x):
-        return self.fc(self.vit(x))
+        feats = self.vit(x)
+        return self.proj(feats)
